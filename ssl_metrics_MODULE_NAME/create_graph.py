06c629d00d8b06db7c0f8b19e1bfe5e307498fc6
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-29 08:33:29 -0500

    Fix indentation

--- ssl_metrics_MODULE_NAME/create_graph.py
+++ ssl_metrics_MODULE_NAME/create_graph.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, Namespace
 
+
 def get_argparse() -> Namespace:
     parser: ArgumentParser = ArgumentParser(
         prog="PROGRAM NAME",
@@ -15,7 +16,7 @@ def get_argparse() -> Namespace:
 
     return parser.parse_args()
   
-  def main() -> None:
+def main() -> None:
     args: Namespace = get_argparse()
 
     if args.input[-5::] != ".json":
