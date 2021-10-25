7911fdaec08116234d623e6968db20e855d09fb3
Matthew Hyatt <mhyatt@luc.edu>
2021-10-25 17:40:26 -0500

    prepare for dataframe mod

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -16,59 +16,71 @@ def get_args():
     args = ap.parse_args()
     return args
 
-# returns data from a loc.json file
+
 def get_data(filename: str) -> list:
+    "returns data from a loc.json file"
     with open(file=filename, mode="r") as file:
         return json.load(file)
 
 
 def team_effort(data) -> int:
+    "returns TE as total elapsed project time"
     return data[len(data) - 1]["day_since_0"]
 
 
 def module_size(data) -> list:
+    "returns a list of delta_loc values"
     return [abs(commit["delta_loc"]) for commit in data]
 
-def get_hash(data) -> list:
-    return [commit["hash"] for commit in data]
 
-def get_day(data) -> list:
-    return [commit["day_since_0"] for commit in data]
+# # not needed
+# def get_hash(data) -> list:
+#     return [commit["hash"] for commit in data]
+#
+# def get_day(data) -> list:
+#     return [commit["day_since_0"] for commit in data]
 
 
 def productivity(MS: list, TE: int) -> list:
+    "calculates prod = (MS as delta loc) / TE"
     return [float(loc / TE) for loc in MS]
 
 
-def write(data:list):
+def authors(data: list) -> set:
+    "returns all known authors of a repo"
+    return set([item["author_email"] for item in data])
+
+def write(data: list):
     "adds the given field as key value pairs to prod.json"
 
-    with open(file='prod.json', mode="w") as file:
+    with open(file="prod.json", mode="w") as file:
         json.dump(data, file)
 
 
 def main():
 
+    # df:DataFrame = pandas.read_json(args.input)
+    'df.to_json()'
+
+
     args = get_args()
-    data = get_data(args.jsonfile.name)
+    data = get_data(args.input.name)
     prod = productivity(module_size(data), team_effort(data))
-    hash = get_hash(data)
-    days = get_day(data)
 
-    '''TODO
-    append prod to the data and return whole dataset
-    output = [... for ... in zip(prod,data)]
-    '''
+    for item, p in zip(data, prod):
+        item["productivity"] = p
 
-    output = [{'productivity':p, 'hash':h, 'day_since_0':d} for p,h,d in zip(prod,hash,days)]
-    write(output)
+    print(len(authors(data)))
+
+    write(data)
+
 
 if __name__ == "__main__":
     main()
 
-'''
+"""
 prod per member as {author email, name}
     prod per member graphed on the same chart
 
 graphs of velocity and acceleration of prod
-'''
+"""
