67f4a1386c78d4577106212ab211e86289fd6b5a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-08 16:55:25 -0600

    Refactor main.py

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, Namespace
 
 import pandas as pd
 from pandas import DataFrame
+from pandas.core.series import Series
 
 
 def get_args() -> Namespace:
@@ -11,11 +12,18 @@ def get_args() -> Namespace:
         description="Productivity is defined as |ΔLOC| / (Team Effort) where Team Effort is the total elapsed time between commits.",
     )
     ap.add_argument(
-        "--input",
         "-i",
+        "--input",
         required=True,
         type=open,
         help="JSON file containing data formatted by ssl-metrics-git-commits-loc extract",
+    )
+    ap.add_argument(
+        "-o",
+        "--output",
+        required=True,
+        type=open,
+        help="JSON file containing data outputted by the application",
     )
 
     args: Namespace = ap.parse_args()
@@ -24,18 +32,22 @@ def get_args() -> Namespace:
 
 def get_prod(df: DataFrame) -> None:
     te: int = df["day_since_0"].max()
-    p: DataFrame = df["delta_loc"].apply(lambda x: abs(x) / te)
+    p: Series = df["delta_loc"].apply(lambda x: abs(x) / te)
     print(type(p))
     df["productivity"] = p
 
 
 def main():
     args = get_args()
+
+    if args.input[-5:] != ".json":
+        print("Input must be a .json file")
+        quit(1)
+
     df: DataFrame = pd.read_json(args.input)
     get_prod(df)
 
-    df.to_json("prod.json")
-
+    df.to_json(args.output)
 
 if __name__ == "__main__":
     main()
