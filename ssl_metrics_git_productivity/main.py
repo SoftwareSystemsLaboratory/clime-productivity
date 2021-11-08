79979a6c15641443f8bf4cb49adf2eb996153a60
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-08 16:49:16 -0600

    Refactor code to be cleaner

--- ssl_metrics_git_productivity/main.py
+++ ssl_metrics_git_productivity/main.py
@@ -1,62 +1,40 @@
-"""takes a loc file as an argument ...
-returns a prod file with calculated productivity"""
+from argparse import ArgumentParser, Namespace
 
-import argparse
-
-import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
 
-def get_args():
-    ap = argparse.ArgumentParser(
+def get_args() -> Namespace:
+    ap:ArgumentParser = ArgumentParser(
         prog="SSL Metrics Git Productivity",
-        usage="Calculates productivity measure of a git project.",
+        usage="Calculates productivity metric of a git project.",
+        description="Productivity is defined as |ΔLOC| / (Team Effort) where Team Effort is the total elapsed time between commits.",
     )
     ap.add_argument(
         "--input",
         "-i",
         required=True,
         type=open,
-        help="...",
+        help="JSON file containing data formatted by ssl-metrics-git-commits-loc extract",
     )
 
-    args = ap.parse_args()
+    args: Namespace = ap.parse_args()
     return args
 
 
-def get_prod(df: DataFrame):
-    te = df["day_since_0"].max()
-    p = df["delta_loc"].apply(lambda x: abs(x) / te)
+def get_prod(df: DataFrame) -> None:
+    te: int = df["day_since_0"].max()
+    p: DataFrame = df["delta_loc"].apply(lambda x: abs(x) / te)
+    print(type(p))
     df["productivity"] = p
 
 
 def main():
-
     args = get_args()
-
     df: DataFrame = pd.read_json(args.input)
     get_prod(df)
-    get_velocity(df)
 
-    "transpose to look pretty"
-    "dont transpose to be effective"
     df.to_json("prod.json")
-    # df.T.to_json("prod.json")
-
-    output = [
-        {"productivity": p, "hash": h, "day_since_0": d}
-        for p, h, d in zip(prod, hash, days)
-    ]
-    write(output)
-
 
 if __name__ == "__main__":
     main()
-
-""" TODO
-prod per member as {author email, name}
-    prod per member graphed on the same chart
-
-graphs of acceleration of prod
-"""
