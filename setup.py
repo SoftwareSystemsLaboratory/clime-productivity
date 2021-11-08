486cb069b28f7e83aacee1d71a619bc99896a6f2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-08 15:36:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-from ssl_metrics_MODULE_NAME import version
+from ssl_metrics_git_productivity import version
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="ssl-metrics-MODULE-NAME",
-    packages=["ssl_metrics_MODULE_NAME"],
+    name="ssl-metrics-git-productivity",
+    packages=["ssl_metrics_git_productivity"],
     version=version.version(),
-    description="SSL Metrics - SHORT DESCRIPTION",
+    description="SSL Metrics - Git Productivity",
     author="Software and Systems Laboratory - Loyola University Chicago",
     author_email="ssl-metrics@ssl.luc.edu",
     license="BSD",
@@ -17,8 +17,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://ssl.cs.luc.edu/projects/metricsDashboard",
     project_urls={
-        "Bug Tracker": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-REPOSITORY-NAME/issues",
-        "GitHub Repository": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-REPOSITORY-NAME",
+        "Bug Tracker": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-git-productivity/issues",
+        "GitHub Repository": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-git-productivity",
     },
     keywords=[
         "git",
@@ -53,8 +53,8 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "ssl-metrics-MODULE-NAME-collect = ssl_metrics_MODULE_NAME.FILENAME:main",
-            "ssl-metrics-MODULE-NAME-graph = ssl_metrics_MODULE_NAME.create_graph:main",
+            "ssl-metrics-git-productivity-collect = ssl_metrics_git_productivity.main:main",
+            "ssl-metrics-git-productivity-graph = ssl_metrics_git_productivity.create_graph:main",
         ]
     },
 )
